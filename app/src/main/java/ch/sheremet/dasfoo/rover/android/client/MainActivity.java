6ad966c428f575d2d563b1ef608956d64b024964
Katarina Sheremet <kate.romanowskaya@gmail.com>
2016-06-01 13:12:04 +0200

    Implement ProviderInstaller in MainActivity

--- app/src/main/java/ch/sheremet/dasfoo/rover/android/client/MainActivity.java
+++ app/src/main/java/ch/sheremet/dasfoo/rover/android/client/MainActivity.java
@@ -1,8 +1,11 @@
 package ch.sheremet.dasfoo.rover.android.client;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -13,9 +16,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.security.ProviderInstaller;
 
-import ch.sheremet.dasfoo.rover.android.client.grpc.authentication.ProviderInstallerListener;
 import ch.sheremet.dasfoo.rover.android.client.grpc.task.AbstractGrpcTaskExecutor;
 import ch.sheremet.dasfoo.rover.android.client.grpc.task.EncodersReadingTask;
 import ch.sheremet.dasfoo.rover.android.client.grpc.task.GettingBoardInfoTask;
@@ -68,11 +71,6 @@ public class MainActivity extends AppCompatActivity {
         mHostEdit = (EditText) findViewById(R.id.host_edit_text);
         mPortEdit = (EditText) findViewById(R.id.port_edit_text);
         mResultText = (TextView) findViewById(R.id.grpc_response_text);
-
-        // Android relies on a security Provider to provide secure network communications.
-        // It verifies that the security provider is up-to-date.
-        ProviderInstaller.installIfNeededAsync(this, new ProviderInstallerListener());
-
     }
 
     @Override
@@ -97,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "You did not enter a host or a port", Toast.LENGTH_SHORT).show();
             return;
         }
-        enableButtons(false);
         new GrpcTask(host, Integer.parseInt(port)).execute(task);
     }
 
@@ -107,7 +104,13 @@ public class MainActivity extends AppCompatActivity {
         mReadEncodersButton.setEnabled(isEnabled);
     }
 
-    public class GrpcTask extends AsyncTask<AbstractGrpcTaskExecutor, Void, String> {
+    public class GrpcTask extends AsyncTask<AbstractGrpcTaskExecutor, Void, String>
+            implements ProviderInstaller.ProviderInstallListener {
+        private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+        private static final String PROVIDER_NOT_INSTALLED =
+                "The security provider isn't available";
+        private static final String PROVIDER_INSTALLED =
+                "The security provider installed successful";
 
         public GrpcTask(final String host, final int port) {
             super();
@@ -115,6 +118,9 @@ public class MainActivity extends AppCompatActivity {
                     || !host.equals(mGrpcConnection.getHost())
                     || port != mGrpcConnection.getPort()) {
                 mGrpcConnection = new GrpcConnection(host, port);
+                // Android relies on a security Provider to provide secure network communications.
+                // It verifies that the security provider is up-to-date.
+                ProviderInstaller.installIfNeededAsync(MainActivity.this, this);
             }
         }
 
@@ -133,6 +139,34 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(final String result) {
             mResultText.setText(result);
             enableButtons(Boolean.TRUE);
+        }
+
+        @Override
+        public void onProviderInstalled() {
+            Toast toast = Toast.makeText(MainActivity.this, PROVIDER_INSTALLED, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        @Override
+        public void onProviderInstallFailed(final int errorCode, final Intent intent) {
+            GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+            if (googleAPI.isUserResolvableError(errorCode)) {
+                googleAPI.getErrorDialog(MainActivity.this, errorCode,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+                return;
+            }
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setMessage(PROVIDER_NOT_INSTALLED);
+            dialog.setCancelable(false);
+            dialog.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(final DialogInterface dialog, final int which) {
+                    dialog.dismiss();
+                    System.exit(0);
+                }
+            });
+            dialog.create();
+            dialog.show();
         }
     }
 }
