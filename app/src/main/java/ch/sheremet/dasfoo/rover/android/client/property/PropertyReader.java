3c8a263435ea24808a28b3a850046b6137cdaa50
Katarina Sheremet <kate.romanowskaya@gmail.com>
2016-06-13 14:36:48 +0200

    Add network property

---
+++ app/src/main/java/ch/sheremet/dasfoo/rover/android/client/property/PropertyReader.java
@@ -0,0 +1,37 @@
+package ch.sheremet.dasfoo.rover.android.client.property;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by Katarina Sheremet on 6/10/16 2:48 PM.
+ */
+
+/** This class is created for temporary purpose. It will be deleted
+ * after development menu with settings.
+ */
+public class PropertyReader {
+    private static final String TAG = PropertyReader.class.getSimpleName();
+    private Context mContext;
+    private Properties mProperties;
+
+    public PropertyReader(final Context context) {
+        this.mContext = context;
+        mProperties = new Properties();
+    }
+
+    public final Properties getProperties(final String file) {
+        try {
+            AssetManager assetManager = mContext.getAssets();
+            InputStream inputStream = assetManager.open(file);
+            mProperties.load(inputStream);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+        return mProperties;
+    }
+}
