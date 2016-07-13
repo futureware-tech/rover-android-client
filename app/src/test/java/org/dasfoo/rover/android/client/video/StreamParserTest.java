12e7a8854c1e697a1866de1d7be0e8125e8178fd
Artem Sheremet <dot.doom@gmail.com>
2016-07-13 20:08:56 +0200

    Use ByteBuffer (instead of byte[]) for takeUnit

--- app/src/test/java/org/dasfoo/rover/android/client/video/StreamParserTest.java
+++ app/src/test/java/org/dasfoo/rover/android/client/video/StreamParserTest.java
@@ -2,6 +2,7 @@ package org.dasfoo.rover.android.client.video;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 import org.dasfoo.rover.android.client.video.StreamParser;
 
@@ -13,12 +14,16 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class StreamParserTest {
-    private byte[] takeUnit(StreamParser p, int maxSize) throws Exception {
-        byte[] unit = new byte[maxSize];
-        int size = p.takeUnit(unit);
-        byte[] sizedUnit = new byte[size];
-        System.arraycopy(unit, 0, sizedUnit, 0, size);
-        return sizedUnit;
+    private byte[] takeUnit(StreamParser p) throws Exception {
+        ByteBuffer b = ByteBuffer.allocate(StreamParser.maxUnitSize);
+        p.takeUnit(b);
+
+        int size = b.position();
+        b.rewind();
+
+        byte[] data = new byte[size];
+        b.get(data);
+        return data;
     }
 
     @Test
@@ -27,7 +32,7 @@ public class StreamParserTest {
             0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
             0x00, 0x00, 0x01,
         }));
-        byte[] unit = this.takeUnit(p, 1024);
+        byte[] unit = this.takeUnit(p);
         assertArrayEquals(new byte[]{0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06}, unit);
     }
 
@@ -37,8 +42,8 @@ public class StreamParserTest {
             0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
             0x00, 0x00, 0x01,
         }));
-        this.takeUnit(p, 1024);
-        this.takeUnit(p, 1024);
+        this.takeUnit(p);
+        this.takeUnit(p);
     }
 
     @Test(expected=IOException.class)
@@ -46,7 +51,7 @@ public class StreamParserTest {
         StreamParser p = new StreamParser(new ByteArrayInputStream(new byte[]{
             0x00, 0x00,
         }));
-        this.takeUnit(p, 1024);
+        this.takeUnit(p);
     }
 
     @Test
@@ -60,17 +65,17 @@ public class StreamParserTest {
             0x00, 0x00, 0x01,
         }));
 
-        byte[] unit = this.takeUnit(p, 1024);
+        byte[] unit = this.takeUnit(p);
         assertArrayEquals(new byte[]{0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06}, unit);
 
-        unit = this.takeUnit(p, 1024);
+        unit = this.takeUnit(p);
         assertArrayEquals(new byte[]{0x00, 0x00, 0x01, 0x05, 0x04, 0x03, 0x02}, unit);
 
-        unit = this.takeUnit(p, 1024);
+        unit = this.takeUnit(p);
         assertArrayEquals(
             new byte[]{0x00, 0x00, 0x01, 0x09, 0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02}, unit);
 
-        unit = this.takeUnit(p, 1024);
+        unit = this.takeUnit(p);
         assertArrayEquals(new byte[]{0x00, 0x00, 0x01}, unit);
     }
 }
